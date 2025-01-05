f99ff0c00f85c664bd91b9b48e69371184b5a81f
LE XUAN VU <5712xuanvu@gmail.com>
2025-01-05 14:00:06 +0700

    update UI and connect
    Add Google and Facebook
    Sync Card Citizen NFT
    Add some UI
    Mint and Update Card, Balance, Address

--- Assets/Web3 Intergrating/EVM/EvmBlockchainManager.cs
+++ Assets/Web3 Intergrating/EVM/EvmBlockchainManager.cs
@@ -1,9 +1,18 @@
+using Nethereum.Contracts;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Numerics;
+using System.Text;
 using System.Threading.Tasks;
 using Thirdweb;
 using Thirdweb.Unity;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 using VitsehLand.Scripts.Pattern.Singleton;
 using WalletConnectUnity.Modal;
 
@@ -11,20 +20,56 @@ namespace Web3Intergrating.Evm
 {
     public class EvmBlockchainManager : Singleton<EvmBlockchainManager>
     {
+        public string CitizenCardContractAddress;
+        public string baseTokenURI = "";
+        public string playerAddress = "";
+
         public UnityEvent<string> OnLoggedIn;
-        public UnityEvent<string> OnModalClosed;
+        public UnityEvent OnModalClosed;
+
+        public GameObject claimPanel;
+        public GameObject playPanel;
+        public Button claimCitizenCardButton;
+        public TextMeshProUGUI claimCitizenCardButtonText;
+        public TextMeshProUGUI balanceText;
+        public RawImage nftImage;
+
+        public bool hasCitizenCard = false;
+        object citizenCardData;
+        public string citizenCardID = "";
+        public CitizenCard citizenCard;
 
         public IThirdwebWallet Wallet { get; private set; }
         public string Address { get; private set; }
 
+        WalletOptions connection = null;
+
+        [System.Serializable]
+        public class CitizenCard
+        {
+            public string name;
+            public string description;
+            public string image;
+            public Properties properties;
+
+            [System.Serializable]
+            public class Properties
+            {
+                public int number;
+                public string name;
+            }
+        }
+
+        public GameObject Group_NFT;
+        public GameObject Group_Main;
+
         private void Start()
         {
-            WalletConnectModal.ModalClosed += (_, _) => OnModalClosed?.Invoke(null);
+            WalletConnectModal.ModalClosed += (_, _) => OnModalClosed?.Invoke();
         }
 
         public async void Login(string authProvider)
         {
-            WalletOptions connection = null;
             AuthProvider provider = AuthProvider.Google;
 
             switch (authProvider)
@@ -50,17 +95,99 @@ namespace Web3Intergrating.Evm
             {
                 connection = new WalletOptions(
                     provider: WalletProvider.InAppWallet,
-                    chainId: 28122024
+                    chainId: 28122024,
+                    inAppWalletOptions: new InAppWalletOptions(authprovider: provider),
+                    smartWalletOptions: new SmartWalletOptions(sponsorGas: true)
                 );
             }
 
             Wallet = await ThirdwebManager.Instance.ConnectWallet(connection);
             Address = await Wallet.GetAddress();
+            playerAddress = Address;
 
-            var balance = await Wallet.GetBalance(28122024);
-            Debug.Log(balance);
+            UpdateBalance();
+
+            var citizenCardContract = await ThirdwebManager.Instance.GetContract(CitizenCardContractAddress, connection.ChainId);
+            var cardBalance = await citizenCardContract.ERC721_BalanceOf(Address);
+            Debug.Log(cardBalance);
 
             OnLoggedIn?.Invoke(Address);
+            if (cardBalance > 0)
+            {
+                hasCitizenCard = true;
+                UpdateCitizenCard(citizenCardContract);
+
+            }
+            else
+            {
+                Debug.Log("Don't have card");
+                playPanel.SetActive(false);
+                claimPanel.SetActive(true);
+            }
+        }
+
+        public async void UpdateBalance()
+        {
+            var balance = await Wallet.GetBalance(connection.ChainId);
+            balanceText.text = "Balance: " + (Thirdweb.Utils.ToEth(balance.ToString())) + " ETH";
+        }
+
+        public async void ClaimCitizenCard()
+        {
+            claimCitizenCardButtonText.text = "Claiming...";
+            claimCitizenCardButton.interactable = false;
+
+            var citizenCardContract = await ThirdwebManager.Instance.GetContract(CitizenCardContractAddress, connection.ChainId);
+            var result = await citizenCardContract.DropERC721_Claim(Wallet, Address, 1);
+
+            UpdateBalance();
+            UpdateCitizenCard(citizenCardContract);
+
+            claimCitizenCardButtonText.text = "Claimed Citizen Card";
+            claimPanel.SetActive(false);
+            playPanel.SetActive(true);
+        }
+
+        public async void UpdateCitizenCard(ThirdwebContract contract)
+        {
+            var ids = await contract.ERC721A_TokensOfOwner(Address);
+            citizenCardID = ids[0].ToString();
+            var base64JsonURI = await contract.ERC721_TokenURI(ids[0]);
+            string citizenCardJsonData = DecodeBase64(GetBase64FromDataUrl(base64JsonURI));
+
+            citizenCard = JsonUtility.FromJson<CitizenCard>(citizenCardJsonData);
+            string imageUrl = citizenCard.image.Replace("ipfs://", baseTokenURI); // Convert IPFS URL
+
+            StartCoroutine(LoadImageFromIPFS(imageUrl));
+        }
+
+        string GetBase64FromDataUrl(string dataUrl)
+        {
+            // Split the string and get the part after "base64,"
+            string[] parts = dataUrl.Split(new[] { "base64," }, StringSplitOptions.None);
+            return parts.Length > 1 ? parts[1] : null; // Return the Base64 part
+        }
+
+        string DecodeBase64(string base64)
+        {
+            byte[] jsonBytes = Convert.FromBase64String(base64);
+            return Encoding.UTF8.GetString(jsonBytes);
+        }
+
+        IEnumerator LoadImageFromIPFS(string url)
+        {
+            UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url);
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Texture image = DownloadHandlerTexture.GetContent(webRequest);
+                nftImage.texture = image;
+            }
         }
 
         internal async Task SubmitScore(float distanceTravelled)
@@ -84,6 +211,12 @@ namespace Web3Intergrating.Evm
             var rank = await contract.Read<int>("getRank", Address);
             Debug.Log($"Rank for address {Address} is {rank}");
             return rank;
+        }
+    
+        public void SwitchGroupUI()
+        {
+            Group_NFT.SetActive(!Group_NFT.activeSelf);
+            Group_Main.SetActive(!Group_Main.activeSelf);
         }
     }
 }
